1307f873af0196f0ab01a6268030f560d0235007
Manuel Garcia <manejarju15@gmail.com>
2023-02-01 10:18:40 -0500

    Iteracion de componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+    frutas = [
+        'Pera',
+        'Sandia', 
+        'Fresa', 
+        'Kiwi',
+        'Mango', 
+        'Manzana'
+    ]
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.frutas.map((fruta) => (
+                        // retorno directo 
+                        <li>{fruta}</li>
+                    ))}
+
+                    {/* /* retorno por defecto
+                    {this.frutas.map((fruta) => {
+                        return (<li>{fruta}</li>)
+                    })}
+                    */ }
+                </ul>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));  
